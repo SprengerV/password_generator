d7edef31182a4017e95047c677d89b43f212cf07
Brendan Francis <74080363+SprengerV@users.noreply.github.com>
2020-12-20 22:02:49 -0500

    Initial commit

---
+++ js/myjava.js
@@ -0,0 +1,5 @@
+var btc = '364i4khD1kfQgHjVv3eitsX97TqmHkie5k'
+var eth = '0x402B70eaf3552c779073D958A38489fc62f52517'
+var doge = 'DT9XgJV7VR6hVAr9nM36oKQ8MnkpJA8kxY'
+var xrp = 'rDa3z4CnfKQy3Sg8CsTfqgk1sErL5wtSao
+var ltc = 'MSnhZBJhyuSTB3W7A4zDFy98ECWXuTQnm3'
